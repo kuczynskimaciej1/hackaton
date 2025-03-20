365ffe3c3f6eef3f0e298f1f0c204812f4999021
rewak <michal.rewak@nokia.com>
2025-03-20 13:39:03 +0100

    SVM Recall: 0.41

--- models/svm.py
+++ models/svm.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
 import joblib
 import os
 
 # Load the dataset
-# Replace 'your_dataset.csv' with the actual dataset file path
-data = pd.read_parquet('dataset_processed.parquet')
+data = pd.read_parquet(r'C:\Users\rewak\git\ghc\private\pythonProject\hackaton\dataset_processed.parquet')
+
+# Limit each class of the 'radio' column to a maximum of 1,000 samples
+data = data.groupby('radio').apply(lambda x: x.sample(n=1000, random_state=42) if len(x) > 1000 else x).reset_index(drop=True)
 
 # Define features (X) and target (y)
-X = data.drop(columns=['Radio'])  # Drop the target column
-y = data['Radio']  # Target column
+X = data.drop(columns=['radio'])  # Drop the target column
+y = data['radio']  # Target column
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -31,8 +33,8 @@ print(f"Model saved to {os.path.abspath(model_path)}")
 y_pred = svm.predict(X_test)
 
 # Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Model Accuracy: {accuracy:.2f}")
+recall = recall_score(y_test, y_pred, average="macro")
+print(f"Model Macro Average Recall: {recall:.2f}")
 
 # Display statistics
 print("Statistics:")
